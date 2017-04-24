8caabd2bf7517c65beba72b147bb9e6c9d87707d
Caroline Davis <caroline_davis@live.com>
2017-04-24 16:37:21 +1000

    feat: Add storyboard design

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,30 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Caroline Davis on 24/04/2017.
+//  Copyright © 2017 Caroline Davis. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
