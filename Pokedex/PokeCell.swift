b37fde3ca08c06e85eed3eeb96910becd2aaf4fe
Caroline Davis <caroline_davis@live.com>
2017-04-24 17:20:38 +1000

    feat: Create custom collection view cell

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,27 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Caroline Davis on 24/04/2017.
+//  Copyright © 2017 Caroline Davis. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
