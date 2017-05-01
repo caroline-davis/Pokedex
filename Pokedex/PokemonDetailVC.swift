45d9b2cd6b989c50ee7f652eaf9c5abf7a1ad55d
Caroline Davis <caroline_davis@live.com>
2017-05-01 16:49:04 +1000

    feat: Linked iboutlets and added ibaction to back button

--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -13,7 +13,18 @@ class PokemonDetailVC: UIViewController {
      var pokemon: Pokemon!
     
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var defenseLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var pokedexLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var attackLabel: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
     
+    @IBOutlet weak var evoLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +32,10 @@ class PokemonDetailVC: UIViewController {
         nameLabel.text = pokemon.name
     }
 
-
+    // take us back
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+ 
 
 }
